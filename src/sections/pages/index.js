08565a33e33b318fcd3df6d3f493aba9f93d9701
leyanessantiago <le.yanessantiago@gmail.com>
2019-01-07 10:53:22 -0500

    feat: implementing Pages page.

--- src/sections/pages/index.js
+++ src/sections/pages/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Card } from 'react-rainbow-components';
+import './styles.css';
 
+const pages = [
+    { name: 'Orders' },
+    { name: 'Users' },
+    { name: 'Followers' },
+    { name: 'Timeline' },
+    { name: 'Gallery' },
+    { name: 'Calendar' },
+    { name: 'Pricing' },
+];
 export default function Pages() {
+    function Cards() {
+        return pages.map((page) => {
+            const src = `assets/images/pages/${page.name.toLowerCase()}.svg`;
+
+            return (
+                <Card className="react-rainbow-admin-pages_card" footer={page.name}>
+                    <img
+                        className="react-rainbow-admin-pages_card-image"
+                        src={src}
+                        alt={`Page ${page.name}`} />
+                </Card>
+            );
+        });
+    }
+
     return (
-        <div className="rainbow-align-content_center rainbow-font-size-heading_x-large">
-            <h1>Pages</h1>
+        <div className="react-rainbow-admin-pages_container">
+            <h1 className="react-rainbow-admin-pages_title">Pages</h1>
+            <p className="react-rainbow-admin-pages_description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <div className="react-rainbow-admin-pages_body">
+                <Cards />
+            </div>
         </div>
     );
 }
