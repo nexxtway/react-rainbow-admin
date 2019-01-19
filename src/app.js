6a6973e75797b3f481788247dea7d9ccb79fa520
TahimiLeonBravo <tahimileon@gmail.com>
2019-01-18 22:47:23 -0600

    feat: change sidebar

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import Application from 'react-rainbow-components/components/Application';
+import Sidebar from 'react-rainbow-components/components/Sidebar';
+import SidebarItem from 'react-rainbow-components/components/SidebarItem';
 import store from './redux/store';
 import Routes from './routes';
 import SectionHeading from './components/SectionHeading';
-import Sidebar from './components/Sidebar';
-import SidebarItem from './components/Sidebar/sidebarItem';
 import {
     ApplicationIcon,
     ChartsIcon,
@@ -20,14 +20,19 @@ import history, { navigateTo } from './history';
 const navStyles = {
     position: 'fixed',
     width: 88,
-    paddingTop: 92,
+    paddingTop: 82,
     backgroundColor: '#fff',
-    minHeight: '100vh',
+    height: '100%',
+    overflowY: 'scroll',
 };
 
 const routerContainer = {
     paddingLeft: 88,
     paddingTop: 68,
+};
+
+const sidebarItem = {
+    marginBottom: 16,
 };
 
 function resolveCurrentUrl() {
@@ -57,13 +62,48 @@ export default class App extends Component {
                         selectedItem={selectedItem}
                         onSelect={this.handleOnSelect}
                         style={navStyles}>
-                        <SidebarItem icon={<DashboardIcon />} name="dashboard" label="Dashboard" onClick={() => navigateTo('/dashboard')} />
-                        <SidebarItem icon={<PagesIcon />} name="pages" label="Pages" onClick={() => navigateTo('/pages')} />
-                        <SidebarItem icon={<ApplicationIcon />} name="applications" label="Applications" onClick={() => navigateTo('/applications')} />
-                        <SidebarItem icon={<PuzzleIcon />} name="components" label="Components" onClick={() => navigateTo('/components')} />
-                        <SidebarItem icon={<MessageIcon />} name="messages" label="Messages" onClick={() => navigateTo('/messages')} />
-                        <SidebarItem icon={<FormsIcon />} name="forms" label="Forms" onClick={() => navigateTo('/forms')} />
-                        <SidebarItem icon={<ChartsIcon />} name="charts" label="Charts" onClick={() => navigateTo('/charts')} />
+                        <SidebarItem
+                            style={sidebarItem}
+                            icon={<DashboardIcon />}
+                            name="dashboard"
+                            label="Dashboard"
+                            onClick={() => navigateTo('/dashboard')} />
+                        <SidebarItem
+                            className="react-rainbow-admin_sidebar-item"
+                            icon={<PagesIcon />}
+                            name="pages"
+                            label="Pages"
+                            onClick={() => navigateTo('/pages')} />
+                        <SidebarItem
+                            className="react-rainbow-admin_sidebar-item"
+                            icon={<ApplicationIcon />}
+                            name="applications"
+                            label="Applications"
+                            onClick={() => navigateTo('/applications')} />
+                        <SidebarItem
+                            className="react-rainbow-admin_sidebar-item"
+                            icon={<PuzzleIcon />}
+                            name="components"
+                            label="Components"
+                            onClick={() => navigateTo('/components')} />
+                        <SidebarItem
+                            className="react-rainbow-admin_sidebar-item"
+                            icon={<MessageIcon />}
+                            name="messages"
+                            label="Messages"
+                            onClick={() => navigateTo('/messages')} />
+                        <SidebarItem
+                            className="react-rainbow-admin_sidebar-item"
+                            icon={<FormsIcon />}
+                            name="forms"
+                            label="Forms"
+                            onClick={() => navigateTo('/forms')} />
+                        <SidebarItem
+                            className="react-rainbow-admin_sidebar-item"
+                            icon={<ChartsIcon />}
+                            name="charts"
+                            label="Charts"
+                            onClick={() => navigateTo('/charts')} />
                     </Sidebar>
                     <div style={routerContainer}>
                         <Routes />
