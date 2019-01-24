0ab7c893ff09cccfa3bc502200fc63f1f3dea0b6
Tahimi <32083556+TahimiLeonBravo@users.noreply.github.com>
2019-01-23 19:30:47 -0600

    fix: fix link dashboard (#24)

--- src/sections/pages/users/index.js
+++ src/sections/pages/users/index.js
@@ -226,7 +226,7 @@ export default class Users extends Component {
                                     type="line"
                                     showLegend={false}
                                     maintainAspectRatio={false}>
-                                    <Dataset values={[370, 90, 950, 530, 800, 960, 650]} borderColor="#1de9b" />
+                                    <Dataset values={[370, 90, 950, 530, 800, 960, 650]} borderColor="#1de9b6" />
                                 </Chart>
                             </div>
                         </Card>
