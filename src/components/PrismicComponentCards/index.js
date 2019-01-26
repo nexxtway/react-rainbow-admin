08656051ee6d45dcb52a2bf63e14dabfe4aec039
cmcartaya <cmcartaya1sm@gmail.com>
2019-01-25 19:32:29 -0500

    fix: fix some comments

--- src/components/PrismicComponentCards/index.js
+++ src/components/PrismicComponentCards/index.js
@@ -1,33 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Card from 'react-rainbow-components/components/Card';
+import PrismicComponentCard from './prismicComponentCard';
 import './styles.css';
-
-function handleOnError(event) {
-    const { receivedEvent } = event.target;
-    receivedEvent.src = './../../../public/assets/images/ComponentDefaultImage';
-}
 
 export default function PrismicComponentCards({ results }) {
     return results.map(({ data }, index) => {
         const key = `component-card-${index}`;
         return (
-            <a
-                className="react-rainbow-admin-component-cards_link"
+            <PrismicComponentCard
                 key={key}
                 href={data['component-url'].url}
-                target="_blank"
-                rel="noopener noreferrer">
-                <Card
-                    className="react-rainbow-admin-component-cards_card"
-                    footer={data['component-name'][0].text}>
-                    <img
-                        src={data['component-image'].url}
-                        onError={handleOnError}
-                        className="react-rainbow-admin-component-cards_card-image"
-                        alt={`Component ${data['component-name'][0].text}`} />
-                </Card>
-            </a>
+                name={data['component-name'][0].text}
+                src={data['component-image'].url} />
         );
     });
 }
---
+++ src/components/PrismicComponentCards/prismicComponentCard.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Card from 'react-rainbow-components/components/Card';
+import './styles.css';
+
+const defaultImage = '/assets/images/component-default-image.svg';
+
+export default class PrismicComponentCard extends Component {
+    constructor(props) {
+        super(props);
+        const { src } = props;
+        this.state = {
+            src: src || defaultImage,
+        };
+        this.handleOnError = this.handleOnError.bind(this);
+    }
+
+    handleOnError() {
+        this.setState({ src: 'assets/images/component-default-image.svg' });
+    }
+
+    render() {
+        const {
+            href,
+            name,
+        } = this.props;
+        const { src } = this.state;
+        return (
+            <a
+                className="react-rainbow-admin-component-cards_link"
+                href={href}
+                target="_blank"
+                rel="noopener noreferrer">
+                <Card
+                    className="react-rainbow-admin-component-cards_card"
+                    footer={name}>
+                    <img
+                        src={src}
+                        onError={this.handleOnError}
+                        className="react-rainbow-admin-component-cards_card-image"
+                        alt={`Component ${name}`} />
+                </Card>
+            </a>
+        );
+    }
+}
+
+PrismicComponentCard.propTypes = {
+    href: PropTypes.string,
+    src: PropTypes.string,
+    name: PropTypes.string,
+};
+
+PrismicComponentCard.defaultProps = {
+    href: undefined,
+    src: undefined,
+    name: undefined,
+};
